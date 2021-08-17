67698207cb8ff58ee72d6852cb37381bce84850a
BAndaru Venkatesh Rao <88892241+BandaruVenkey@users.noreply.github.com>
2021-08-17 11:29:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import phonenumbers
-# from number import number
 
+#to take user input eg. +91phonenumber
 number = input("Enter the phone number with country code")
 
+#code for the location
 from phonenumbers import geocoder
 ch_number = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(ch_number, "en"))
 
-
+#code for service provider
 from phonenumbers import  carrier
 service_number = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_number, "en"))
